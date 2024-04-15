f60c4a4ebebff0499bc483758d12a6eefc776855
Biraj ST <birajs.2002@gmail.com>
2024-04-15 18:34:30 +0545

    Revert "Update commit"
    This reverts commit 4e7ab29c8ab11eb71e08dea57e447cc602420f01.

--- utils/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'startsession.php';
-    unset($_SESSION['user']);
-    header('location: ../login.php');
-?>
--- utils/strip_string.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function e($val) {
-    global $db;
-    return mysqli_real_escape_string($db, trim($val));
-}
-
-?>
